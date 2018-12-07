50db918d69658e7213f7eeed27318daddf099b7b
TAKDANAI JIRAWANICHKUL <takdanai.3473@mail.kmutt.ac.th>
2018-12-07 18:40:16 +0700

    home001

--- Online-Movie-Reservation.c
+++ Online-Movie-Reservation.c
@@ -1,10 +1,7 @@
 /*
  * CPE326 - Operating Systems
- *
  * Online Movie Reservation
- *
  * Group: PAJTN
- *
  * Phutthitham Kunapanyakorn  CPE 59070503439 phutthitham@gmail.com
  * Anchisa     Yodkaew        CPE 59070503465 kppooke@gmail.com
  * Jarit       Laoprasert     CPE 59070503466 franksam014@gmail.com
@@ -12,34 +9,81 @@
  * Natcha      Klangplemchitt CPE 59070503487 natchaploy9@gmail.com
  *
  */
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
-int seat[5][5];
+#define MAXSEAT_ROW 5
+#define MAXSEAT_COLUMN 5
 
-void initialize()
+int seat[MAXSEAT_ROW][MAXSEAT_COLUMN];
+void initialize_Seat()
 {
-	int i=0;
-	int j=0;
-	
-	printf("+---+---+---+---+---+\n");
-	
-	for(i=0;i<5;i++)
+	int row = 0;
+	int column = 0;
+	for(row = 0; row < MAXSEAT_ROW ; row++)
 	{
-		for(j=0;j<5;j++)
+		for(column = 0;column < 5;column++)
 		{
-			seat[i][j]=0;
-			printf("| %d ",seat[i][j]);
+			seat[row][column] = 0;
 		}
-		
+	}
+}
+void printSeat()
+{
+	int row = 0;
+	int column = 0;
+	printf("+---+---+---+---+---+\n");
+	for(row = 0; row < MAXSEAT_ROW ; row++)
+	{
+		for(column = 0;column < 5;column++)
+		{
+			printf("| %d ",seat[row][column]);
+		}
 		printf("|\n+---+---+---+---+---+\n");
 	}
 }
 
+void generateFile()
+{
+	printf("\nGenerateFile function\n");
+}
+void runSimulation()
+{
+	printf("\nRun simulation function\n");
+}
+void testSimulation()
+{
+	printf("\nTest simulation function\n");
+}
+
 int main()
 {
-	initialize();
+	char input[64];
+	int userChoice = 0; 
+	initialize_Seat();
+	printf("Online-Movie-Reservation System\n");
+	do {
+		printf("\n\nSelect what you want to do\n");
+		printf("1:Generate file\n2:Run simulation\n3:Test result\n4:Print Seat\n0:Exit Program\nInput number : ");
+		fgets(input,sizeof(input),stdin);
+	 	sscanf(input,"%d",&userChoice);	
+	 	if (userChoice == 1)
+	 		generateFile();
+	 	else if (userChoice == 2)
+	 		runSimulation();
+	 	else if (userChoice == 3)
+	 		testSimulation();
+	 	else if (userChoice == 4)
+	 		printSeat();
+	 	else if (userChoice > 4)
+	 		printf("Enter wrong choice Please enter it again.\n");
+	 	else
+	 		printf("End program Thankyou! Goodbye!\n");
+	} while(userChoice > 0);
+	// seat[0][1] = 1;
+	// seat[2][1] = 1;
+	// seat[4][3] = 1;
 	return 0;
 }
+
